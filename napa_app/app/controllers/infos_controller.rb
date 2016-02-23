d657f97081fa8fad612e715c7e7eade471d89614
nscalf561 <scalf.nicholas@gmail.com>
2016-02-23 15:51:07 -0800

    Seeds the database

--- napa_app/app/controllers/infos_controller.rb
+++ napa_app/app/controllers/infos_controller.rb
@@ -1,10 +1,15 @@
 class InfosController < ApplicationController
 
-  # def index
-  #   @user = User.find(params[:user_id])
-  #   @wines = @user.wines
+	# def create
+	# 	user_params = params.require(:user).permit(:username, :password, :preferences, :email)
+	# 	user = User.new(user_params)
+	# 	if user.save
+	# 		session[:user_id] = user.id
+	# 		redirect_to user_path(user)
+	# 	else 
+	# 		redirect_to "/login"
+	# 	end
+	# end
 
-  #   render :index
-  # end
 
 end
--- napa_app/db/seeds.rb
+++ napa_app/db/seeds.rb
@@ -1,7 +1,69 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.destroy_all
+Wine.destroy_all
+Info.destroy_all
+# Let's create a bunch of records
+User.create([
+  {
+    username: "makethiswitty",
+    password: "password0",
+    preferences: "Red",
+    email: "makingseeds@gmail.com"
+  },
+  {
+    username: "thisstillisntwitty",
+    password: "password1",
+    preferences: "White",
+    email: "secondseed@gmail.com"
+  },
+  {
+    username: "thewitisalie",
+    password: "password2",
+    preferences: "Rose",
+    email: "gmailorbust@gmail.com"
+  }
+])
+
+Wine.create([
+	{
+    vineyard: "Breggo",
+    year: "1985",
+    name: "Some Red Wine",
+    color: "Red",
+    notes: " ",
+    link: " "
+	},
+	{
+    vineyard: "Elke Vineyards",
+    year: "1984",
+    name: "Some White Wine",
+    color: "White",
+    notes: " ",
+    link: " "
+	},
+	{
+    vineyard: "Navarro",
+    year: "2003",
+    name: "Some Rose Wine",
+    color: "Rose",
+    notes: " ",
+    link: " "
+	}
+])
+
+Info.create([
+	{
+		review: 4,
+		textNotes: "here are some notes"
+	},
+	{
+		review: 2,
+		textNotes: "A second pair of notes"
+	},
+	])
+
+# USERS: (:username, :password, :preferences, :email)
+# WINES: (:vineyard, :year, :name, :color, :notes, :link)
+# INFOS: (:review, :textNotes)
