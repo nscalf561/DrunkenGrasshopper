a894cb59c9965208ad4d66be111cc336f3c1dd0e
nscalf561 <scalf.nicholas@gmail.com>
2016-02-25 17:04:34 -0800

    Mostly styled, still looks bad

--- napa_app/app/controllers/wines_controller.rb
+++ napa_app/app/controllers/wines_controller.rb
@@ -7,15 +7,10 @@ class WinesController < ApplicationController
 	end
 
 	def create
-		wine_params = params.require(:wine).permit(:vineyard, :year, :name, :color, :notes, :link)
+		wine_params = params.require(:wine).permit(:name, :vineyard, :year, :color, :notes, :link)
 		wine = Wine.create(wine_params)
 		if wine.save
 			redirect_to "/wines"
-		end
-		info_params = params.require(:info).permit(:review, :textNotes, :user_id, :wine_id)
-		info = Info.create(info_params)
-		if info.save
-			redirect_to "/infos"
 		end
 
 	end
